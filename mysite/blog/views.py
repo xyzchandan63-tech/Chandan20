82a86baf02d1b6f5b9ec99558b5dc18c092806db
chandan <xyzchandan63@gmail.com>
2026-03-28 15:58:24 +0530

    abc

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,6 +6,7 @@ from .forms import CommentForm, EmailPostForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -40,7 +41,14 @@ def post_detail(request, year, month, day, post):
         )
     comments = post.comments.filter(active=True)
     form = CommentForm()
-    return render(request, 'blog/post/detail.html', {'post': post,'comments':comments, 'form':form})
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts=Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similer_posts=similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags','-publish')[:4]
+    return render(request, 'blog/post/detail.html', {'post': post,'comments':comments, 'form':form,'similar_posts':similar_posts})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
