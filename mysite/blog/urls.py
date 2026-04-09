a48e52e04e797e948db8111e561b45a18349d3ab
chandan <xyzchandan63@gmail.com>
2026-04-09 17:58:49 +0530

    april-9

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
 from . import views
+from .feeds import LatestPostFeed
 app_name='blog'
 urlpatterns=[
     path('',views.post_list,name='post_list'),
@@ -12,4 +13,5 @@ urlpatterns=[
     path('<int:post_id>/comment/',views.post_comment,name='post_comment'),
     path('',views.post_list,name='post_list'),
     path('tag/<slug:tag_slug>/',views.post_list,name='post_list_by_tag'),
+    path('feed/',LatestPostFeed(),name='post_feed'),
 ]
